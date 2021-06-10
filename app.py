f8e8f017506fbb2391d0233ac63b862bc6253623
flovearth <flovearth@protonmail.com>
2021-06-10 15:06:36 +0100

    upfate

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import os
 
 ## Number of days you want to make commits
-for i in range(1,200,9):
+for i in range(11,200,9):
     d = str(i) + ' check the code tasks to be done:'
     ## Open a text file and modify it
-    with open('Stage3Updates.txt', 'a') as file:
+    with open('StageUpdates.txt', 'a') as file:
         file.write(d+'\n')
     ## Add CodeTasksToDo.py to staging area
     os.system('git add StageUpdates.txt')
